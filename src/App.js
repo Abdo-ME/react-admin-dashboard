bbbe44688471ce45069b4cffd186275da0b03e8a
Abdo-ME <abdelhakmehda@gmail.com>
2022-03-11 17:06:54 +0100

    handeling Sidebar component

--- src/App.js
+++ src/App.js
@@ -15,27 +15,24 @@ import Single from "./pages/single/Single";
 function App() {
   return (
     <div className="App">
-      
     <BrowserRouter>
-    <Routes>
-          <Route path="/" >
-            <Route index element={<Home/>}/>
-            <Route path="login" element={ <Login/> } />
-            <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element ={<Single/>}/>
-              <Route path="new" element ={<New/>}/>
-            </Route>
-            <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element ={<Single/>}/>
-              <Route path="new" element ={<New/>}/>
-            </Route>
-              
-            
+      <Routes>
+        <Route path="/" >
+          <Route index element={<Home/>}/>
+          <Route path="login" element={ <Login/> } />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element ={<Single/>}/>
+            <Route path="new" element ={<New/>}/>
           </Route>
-    </Routes>
-  </BrowserRouter>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element ={<Single/>}/>
+            <Route path="new" element ={<New/>}/>
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
