3fb54b4d4a58f43c55ec55e59dc7fc3c5a889232
Abdo-ME <abdelhakmehda@gmail.com>
2022-03-18 23:19:34 +0100

    add Dark Mode

--- src/components/dataTable/DataTable.js
+++ src/components/dataTable/DataTable.js
@@ -2,23 +2,29 @@ import './dataTable.scss'
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 
 const DataTable = ({ title ,pathName}) => {
+    const [data, setData] = useState(userRows);
 
+    const handleDelete = (id) => {
+        setData(data.filter((item) => item.id !== id));
+        
+    };
     const actionCollumn = [
         {
             field: "action",
             headerName: "Action",
             width: 200,
-            renderCell: () => {
+            renderCell: (params) => {
                 return (
                     <div className="cellAction">
                         <Link to="/users/test">
                         <div className="viewButton">View</div>
                         </Link>
-                        <div className="deleteButton">Delete</div>
+                        <div className="deleteButton"  onClick={() => handleDelete(params.row.id)}>Delete</div>
                     </div>
                 )
             }
@@ -33,7 +39,7 @@ return (
             </Link>
         </div>
         <DataGrid className="datagrid"
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionCollumn)}
         pageSize={8}
         rowsPerPageOptions={[8]}
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,13 +2,18 @@ import "./navbar.scss"
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightModeIcon from '@mui/icons-material/LightMode';
 import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 
 const Navbar = () => {
+  const { dispatch,darkMode } = useContext(DarkModeContext);
+
   return (
     <div className='navbar'>
       <div className="wrapper">
@@ -21,8 +26,9 @@ const Navbar = () => {
             <LanguageOutlinedIcon className="icon" />
             English
           </div>
-          <div className="item">
-            <DarkModeOutlinedIcon className="icon"/>
+          <div className="item" onClick={()=>dispatch({type: "TOGGLE"})}>
+            {darkMode?<LightModeIcon  className="icon"/>: <DarkModeOutlinedIcon className="icon"/>}
+            
           </div>
           <div className="item">
             <FullscreenExitOutlinedIcon className="icon"/>
